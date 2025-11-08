bac928a329919f1cf53e0bea00d6a2640a55b687
rootankush <rootankushprompt@gmail.com>
2025-11-08 17:50:19 +0530

    Update code added dom creatBookCar

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
 const submitBtn = document.getElementById("submitBtn");
+const display = document.getElementById("display-container");
 const myLibrary = [];
 
 function Book(formTitle, formAuthor, formPages, formRead) {
-    this.formTitle = formTitle;
-    this.formAuthor = formAuthor;
-    this.formPages = formPages;
-    this.formRead = formRead;
-    this.uuid = crypto.randomUUID();
+	this.formTitle = formTitle;
+	this.formAuthor = formAuthor;
+	this.formPages = formPages;
+	this.formRead = formRead;
+	this.uuid = crypto.randomUUID();
 }
 
-submitBtn.addEventListener("click", function (e) {
-    e.preventDefault();
-    const formTitle = document.getElementById("title").value;
-    const formAuthor = document.getElementById("author").value;
-    const formPages = document.getElementById("pages").value;
-    const formRead = document.getElementById("read").value;
-    const book = new Book(formTitle, formAuthor, formPages, formRead);
-    myLibrary.push(book);
+submitBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	const formTitle = document.getElementById("title").value;
+	const formAuthor = document.getElementById("author").value;
+	const formPages = document.getElementById("pages").value;
+	const formRead = document.getElementById("read").checked;
+	const book = new Book(formTitle, formAuthor, formPages, formRead);
+	myLibrary.push(book);
 });
+
+const createBookCard = (book) => {
+	const bookCard = document.createElement('div')
+	const title = document.createElement('p')
+	const author = document.createElement('p')
+	const pages = document.createElement('p')
+	const buttonGrp = document.createElement('div')
+	const readBtn = document.createElement('button')
+	const removeBtn = document.createElement('button')
+}
