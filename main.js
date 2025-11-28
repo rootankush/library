c8438f8f76967bfb7bb48adc67d6ae92ac5b001a
rootankush <rootankushprompt@gmail.com>
2025-11-28 18:09:55 +0530

    bruh

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const submitBtn = document.getElementById("submitBtn");
+const submitBtn = document.getElementById("btn-submit");
 const display = document.getElementById("display-container");
 const myLibrary = [];
+const formTitle = document.getElementById("title");
+const formAuthor = document.getElementById("author");
+const formPages = document.getElementById("pages");
+const formRead = document.getElementById("read");
 
 function Book(formTitle, formAuthor, formPages, formRead) {
 	this.formTitle = formTitle;
@@ -10,22 +14,38 @@ function Book(formTitle, formAuthor, formPages, formRead) {
 	this.uuid = crypto.randomUUID();
 }
 
-submitBtn.addEventListener("click", (e) => {
-	e.preventDefault();
-	const formTitle = document.getElementById("title").value;
-	const formAuthor = document.getElementById("author").value;
-	const formPages = document.getElementById("pages").value;
-	const formRead = document.getElementById("read").checked;
+function addBookToLibrary() {
+	formTitle.value;
+	formAuthor.value;
+	formPages.value;
+	formRead.checked;
 	const book = new Book(formTitle, formAuthor, formPages, formRead);
 	myLibrary.push(book);
-});
-
-const createBookCard = (book) => {
-	const bookCard = document.createElement('div')
-	const title = document.createElement('p')
-	const author = document.createElement('p')
-	const pages = document.createElement('p')
-	const buttonGrp = document.createElement('div')
-	const readBtn = document.createElement('button')
-	const removeBtn = document.createElement('button')
 }
+
+function displayBook() {
+	const title = document.createElement('h3') 
+	const author = document.createElement('p') 
+	const pages = document.createElement('p') 
+	const readStatus = document.createElement('p') 
+
+	title.textContent = `Title: ${formTitle.value}`
+	author.textContent = `Author: ${formAuthor.value}`
+	pages.textContent = `Pages: ${formPages.value}`
+	if(formRead.checked === true) {
+		readStatus.textContent = "Did you read the Book: Yes"
+	}
+	else {
+		readStatus.textContent = "Did you read the Book: No"
+	}
+
+	const card = document.createElement('div')
+	card.append(title, author, pages, readStatus)
+
+	display.appendChild(card)
+}
+
+submitBtn.addEventListener("click", () => {
+	addBookToLibrary()
+	displayBook()
+})
