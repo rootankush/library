771da17963cbbb01da87e928f29aac3f1ded1312
rootankush <rootankushprompt@gmail.com>
2025-11-30 11:11:16 +0530

    mostly done

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const myLibrary = [];
 const formTitle = document.getElementById("title");
 const formAuthor = document.getElementById("author");
 const formPages = document.getElementById("pages");
-const formRead = document.getElementById("read");
+const formRead = document.getElementById("readStatus");
 let popup = document.getElementById("popup");
 
 function Book(formTitle, formAuthor, formPages, formRead) {
@@ -37,24 +37,76 @@ function displayBook() {
 	const author = document.createElement('p') 
 	const pages = document.createElement('p') 
 	const readStatus = document.createElement('p') 
+	const buttonDiv = document.createElement('div')
+	const toggleRead = document.createElement('button')
+	const removeRead = document.createElement('button')
 
-	title.textContent = `Title: ${formTitle.value}`
-	author.textContent = `Author: ${formAuthor.value}`
-	pages.textContent = `Pages: ${formPages.value}`
+	let text = document.createTextNode("Toggle Read")
+	let remove = document.createTextNode("Remove")
+
+	title.textContent = `${formTitle.value}`
+	author.textContent = `Author ${formAuthor.value}`
+	pages.textContent = `Pages ${formPages.value}`
 	if(formRead.checked === true) {
-		readStatus.textContent = "Did you read the Book: Yes"
+		readStatus.textContent = "Already read"
 	}
 	else {
-		readStatus.textContent = "Did you read the Book: No"
+		readStatus.textContent = "Haven't read yet"
 	}
 
+	toggleRead.append(text);
+	removeRead.append(remove);
+
+	buttonDiv.append(toggleRead, removeRead)
+
 	const card = document.createElement('div')
-	card.append(title, author, pages, readStatus)
+	card.append(title, author, pages, readStatus, buttonDiv)
+
+	removeRead.addEventListener("click", () => {
+		card.remove();
+	})
+
+	toggleRead.addEventListener("click", () => {
+
+	})
+
+	//styling dom elements
+
+	buttonDiv.style.justifyContent = "space-between"
+
+	// style toggle Button
+	toggleRead.style.background = "#262d38ff";
+	toggleRead.style.color = "#ffffff";
+	toggleRead.style.padding = "0.5rem";
+	toggleRead.style.borderRadius = "0.7rem";
+	toggleRead.style.margin = "1rem 0.5rem";
+	toggleRead.style.boxShadow = "2px 2px 2px rgba(0, 0, 0, 0.5)";
+
+	// style remove button
+	removeRead.style.background = "red";
+	removeRead.style.color = "#fff";
+	removeRead.style.padding = "0.5rem";
+	removeRead.style.borderRadius = "0.7rem";
+	removeRead.style.margin = "1rem 0.5rem";
+
+	// style card
+	title.style.fontSize = "1.5rem";
+	card.style.background = "#393E46";
+	card.style.color = "#fff";
+	card.style.margin = "0.8rem";
+	card.style.border = "1px solid #ffffff"
+	card.style.borderRadius = "1rem"
+	card.style.padding = "1rem"
+	card.style.fontSize = "1rem"
+	card.style.textAlign = "start"
+	card.style.lineHeight = "2.5rem"
 
 	display.appendChild(card)
 }
 
-submitBtn.addEventListener("click", () => {
+submitBtn.addEventListener("click", (e) => {
+	e.preventDefault();
 	addBookToLibrary()
 	displayBook()
 })
+
