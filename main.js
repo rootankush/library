ffaf7ed8c474e6e5c4f5f2abe34b699f91838784
rootankush <rootankushprompt@gmail.com>
2026-05-12 03:47:41 +0530

    form validation

--- main.js
+++ main.js
@@ -5,116 +5,148 @@ const formTitle = document.getElementById("title");
 const formAuthor = document.getElementById("author");
 const formPages = document.getElementById("pages");
 const formRead = document.getElementById("readStatus");
+const titleError = document.getElementById("titleError");
+const authorError = document.getElementById("authorError");
+const pagesError = document.getElementById("pagesError");
+
 let popup = document.getElementById("popup");
 
 class Book {
-	constructor({ formTitle, formAuthor, formPages, formRead }) {
-		this.formTitle = formTitle;
-		this.formAuthor = formAuthor;
-		this.formPages = formPages;
-		this.formRead = formRead;
-		this.formuuid = crypto.randomUUID();
-	}
+  constructor({ formTitle, formAuthor, formPages, formRead }) {
+    this.formTitle = formTitle;
+    this.formAuthor = formAuthor;
+    this.formPages = formPages;
+    this.formRead = formRead;
+    this.formuuid = crypto.randomUUID();
+  }
 }
 
 function addBookToLibrary() {
-	formTitle.value;
-	formAuthor.value;
-	formPages.value;
-	formRead.checked;
-	const book = new Book(formTitle, formAuthor, formPages, formRead);
-	myLibrary.push(book);
+  formTitle.value;
+  formAuthor.value;
+  formPages.value;
+  formRead.checked;
+  const book = new Book(formTitle, formAuthor, formPages, formRead);
+  myLibrary.push(book);
 }
 
 function openPopup() {
-	popup.classList.add("open-popup");
+  popup.classList.add("open-popup");
 }
 
 function closePopup() {
-	popup.classList.remove("open-popup");
+  popup.classList.remove("open-popup");
 }
 
 function displayBook() {
-	const title = document.createElement("h3");
-	const author = document.createElement("p");
-	const pages = document.createElement("p");
-	const readStatus = document.createElement("p");
-	const buttonDiv = document.createElement("div");
-	const toggleRead = document.createElement("button");
-	const removeBook = document.createElement("button");
+  const title = document.createElement("h3");
+  const author = document.createElement("p");
+  const pages = document.createElement("p");
+  const readStatus = document.createElement("p");
+  const buttonDiv = document.createElement("div");
+  const toggleRead = document.createElement("button");
+  const removeBook = document.createElement("button");
 
-	let text = document.createTextNode("Toggle Read");
-	let remove = document.createTextNode("Remove");
+  let text = document.createTextNode("Toggle Read");
+  let remove = document.createTextNode("Remove");
 
-	title.textContent = `${formTitle.value}`;
-	author.textContent = `Author: ${formAuthor.value}`;
-	pages.textContent = `Pages: ${formPages.value}`;
-	if (formRead.checked === true) {
-		readStatus.textContent = "Already read";
-	} else {
-		readStatus.textContent = "Haven't read yet";
-	}
+  title.textContent = `${formTitle.value}`;
+  author.textContent = `Author: ${formAuthor.value}`;
+  pages.textContent = `Pages: ${formPages.value}`;
+  if (formRead.checked === true) {
+    readStatus.textContent = "Already read";
+  } else {
+    readStatus.textContent = "Haven't read yet";
+  }
 
-	toggleRead.append(text);
-	removeBook.append(remove);
+  toggleRead.append(text);
+  removeBook.append(remove);
 
-	buttonDiv.append(toggleRead, removeBook);
+  buttonDiv.append(toggleRead, removeBook);
 
-	const card = document.createElement("div");
-	card.append(title, author, pages, readStatus, buttonDiv);
+  const card = document.createElement("div");
+  card.append(title, author, pages, readStatus, buttonDiv);
 
-	removeBook.addEventListener("click", () => {
-		card.remove();
-	});
+  removeBook.addEventListener("click", () => {
+    card.remove();
+  });
 
-	toggleRead.addEventListener("click", () => {
-		if (readStatus.textContent === "Already read") {
-			readStatus.textContent = "Haven't read yet";
-		} else {
-			readStatus.textContent = "Already read";
-		}
-	});
+  toggleRead.addEventListener("click", () => {
+    if (readStatus.textContent === "Already read") {
+      readStatus.textContent = "Haven't read yet";
+    } else {
+      readStatus.textContent = "Already read";
+    }
+  });
 
-	//styling dom elements
+  //styling dom elements
 
-	buttonDiv.style.justifyContent = "space-between";
+  buttonDiv.style.justifyContent = "space-between";
 
-	// style toggle Button
-	toggleRead.style.background = "#262d38ff";
-	toggleRead.style.color = "#ffffff";
-	toggleRead.style.padding = "0.5rem";
-	toggleRead.style.borderRadius = "0.7rem";
-	toggleRead.style.margin = "1rem 0.5rem";
-	toggleRead.style.boxShadow = "2px 2px 2px rgba(0, 0, 0, 0.5)";
+  // style toggle Button
+  toggleRead.style.background = "#262d38ff";
+  toggleRead.style.color = "#ffffff";
+  toggleRead.style.padding = "0.5rem";
+  toggleRead.style.borderRadius = "0.7rem";
+  toggleRead.style.margin = "1rem 0.5rem";
+  toggleRead.style.boxShadow = "2px 2px 2px rgba(0, 0, 0, 0.5)";
 
-	// style remove button
-	removeBook.style.background = "red";
-	removeBook.style.color = "#fff";
-	removeBook.style.padding = "0.5rem";
-	removeBook.style.borderRadius = "0.7rem";
-	removeBook.style.margin = "1rem 0.5rem";
+  // style remove button
+  removeBook.style.background = "red";
+  removeBook.style.color = "#fff";
+  removeBook.style.padding = "0.5rem";
+  removeBook.style.borderRadius = "0.7rem";
+  removeBook.style.margin = "1rem 0.5rem";
 
-	// style card
-	title.style.fontSize = "1.5rem";
-	card.style.background = "#393E46";
-	card.style.color = "#fff";
-	card.style.margin = "0.8rem";
-	card.style.border = "1px solid #ffffff";
-	card.style.borderRadius = "1rem";
-	card.style.padding = "1rem";
-	card.style.fontSize = "1rem";
-	card.style.textAlign = "start";
-	card.style.lineHeight = "2.5rem";
+  // style card
+  title.style.fontSize = "1.5rem";
+  card.style.background = "#393E46";
+  card.style.color = "#fff";
+  card.style.margin = "0.8rem";
+  card.style.border = "1px solid #ffffff";
+  card.style.borderRadius = "1rem";
+  card.style.padding = "1rem";
+  card.style.fontSize = "1rem";
+  card.style.textAlign = "start";
+  card.style.lineHeight = "2.5rem";
 
-	display.appendChild(card);
+  display.appendChild(card);
 }
 
 const form = document.getElementById("form");
 
 submitBtn.addEventListener("click", (e) => {
-	e.preventDefault();
-	addBookToLibrary();
-	displayBook();
-	closePopup();
-	form.reset();
+  let error = false;
+  if (formTitle.validity.valueMissing) {
+    titleError.textContent = "Title needed";
+    error = true;
+  } else {
+    titleError.textContent = "";
+  }
+
+  if (formAuthor.validity.valueMissing) {
+    authorError.textContent = "Author needed";
+    error = true;
+  } else {
+    authorError.textContent = "";
+  }
+
+  if (formPages.validity.valueMissing) {
+    pagesError.textContent = "Pages needed";
+    error = true;
+  } else if (formPages.validity.typeMismatch) {
+    pagesError.textContent = "input should be number";
+    error = true;
+  } else {
+    pagesError.textContent = "";
+  }
+
+  if (error) {
+    e.preventDefault();
+  } else {
+    addBookToLibrary();
+    displayBook();
+    closePopup();
+    form.reset();
+  }
 });
